b7fd9048d4bc3b3385e50c72859737c23ac949e9
Gaurav-Shende-93 <githubcontol@gmail.com>
2025-07-04 20:56:15 +0530

    I add the program for browser so we can take input from user

--- table.js
+++ table.js
@@ -1,6 +1,17 @@
-n = 2;
+let n = 2;
 
 for (i = n; i <= n * 10; i = i + n) {
     console.log(i);
 }
+
+
+// *for browser to get input from user*
+
+// let n = prompt("Enter The Number of Table :- ");
+
+// n = parseInt(n);
+
+// for (i = n; i <= n * 10; i = i + n) {
+//     console.log(i);
+// }
 
